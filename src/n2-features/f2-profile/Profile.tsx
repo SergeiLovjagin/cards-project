856bfc6182c7309beabaf68bb483a5b90c0a04d4
SergeiLovjagin <serlovjagin@gmail.com>
2021-06-01 12:18:33 +0300

    Logout request (need to finish). Created a user authorization check on the profile page. If there are no cookies, then redirecting to the login form

--- src/n2-features/f2-profile/Profile.tsx
+++ src/n2-features/f2-profile/Profile.tsx
@@ -1,28 +1,33 @@
 import {useDispatch, useSelector} from "react-redux";
 import {RootReducerType} from "../../n1-main/m2-bll/store";
 import {ProfileType} from "../../n1-main/m3-dal/api";
-import {authMe, setError} from "../../n1-main/m2-bll/profileReducer";
+import {authMe} from "../../n1-main/m2-bll/profileReducer";
 import React, {useEffect} from "react";
+import {Redirect} from "react-router-dom";
+import {PATH} from "../../n1-main/m1-ui/routes/Routes";
 
 export const Profile = () => {
     //HOOKS
     const user = useSelector<RootReducerType, ProfileType>(state => state.profile.user)
-    const {loading, success, error} = useSelector<RootReducerType, { loading: boolean, success: boolean, error: string }>(state => state.profile);
+    const {loading, success, profileError} = useSelector<RootReducerType, { loading: boolean, success: boolean, profileError: string}>(state => state.profile);
+    const user_id = useSelector<RootReducerType,string>(state => state.profile.user._id)
     const dispatch = useDispatch()
+
     useEffect(() => {
-        if (!success) {
+        if (user_id === undefined) {
             dispatch(authMe())
         }
-        return function cleanup() {
-            dispatch(setError(''))
-        }
-    }, [success, dispatch])
+    }, [user_id])
+
+    if (success && user_id === undefined) {
+        return <Redirect to={PATH.LOGIN}/>
+    }
 
     return (
         <div>
             {loading && <div>LOADING......</div>}
-            {error.length > 0 && <div>{error}</div>}
-            {success &&
+            {profileError.length > 0 && <div>{profileError}</div>}
+            {success && user_id &&
             <>
                 Profile Page
                 {Object.entries(user)
