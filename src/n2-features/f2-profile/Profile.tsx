a4c47f2b161f11d6326973389047530914bc790d
TrocyukS96 <trotzuk.stanislav@gmail.com>
2021-06-10 19:59:35 +0300

    changes in component Profile

--- src/n2-features/f2-profile/Profile.tsx
+++ src/n2-features/f2-profile/Profile.tsx
@@ -7,12 +7,17 @@ import {Redirect} from "react-router-dom";
 import {PATH} from "../../n1-main/m1-ui/routes/Routes";
 import {SuperInputText} from "../../n1-main/m1-ui/common/SuperInput/SuperInputText";
 import {PopUp} from "../../n1-main/m1-ui/common/utills/modal-popUp/PopUp";
+import s from "./Profile.module.scss";
 
 export const Profile = React.memo(() => {
     //HOOKS
     const dispatch = useDispatch()
     const user = useSelector<RootReducerType, ProfileType | null>(state => state.profile.user)
-    const {loading, success, profileError} = useSelector<RootReducerType, { loading: boolean, success: boolean, profileError: string }>(state => state.profile);
+    const {
+        loading,
+        success,
+        profileError
+    } = useSelector<RootReducerType, { loading: boolean, success: boolean, profileError: string }>(state => state.profile);
 
     //UPDATING PROFILE
     const [change, setChange] = useState(false)
@@ -71,36 +76,41 @@ export const Profile = React.memo(() => {
         return <Redirect to={PATH.LOGIN}/>
     }
     return (
-        <div>
-            {loading && <div>LOADING......</div>}
-            {
-                profileError.length > 0 && <PopUp setServerErrorCallback={onClosePopUpHandler} text={profileError}/>
-            }
-            {/* user && profileError.length === 0 &&*/}
-            {success &&
-            <>
-                <div>
-                    <img style={{width: '100px', height: '100px'}} src={user ? user.avatar : ''} alt={'userPhoto'}/>
-                </div>
-                {change &&
-                <div>
-                    <input type='file' onChange={event => uploadImage(event)}/>
-                </div>
+        <div className={s.profile}>
+            <div className={s.wrap}>
+                {loading && <div>LOADING......</div>}
+                {
+                    profileError.length > 0 && <PopUp setServerErrorCallback={onClosePopUpHandler} text={profileError}/>
                 }
-
-                {!change
-                    ? <div>{user && user.name}</div>
-                    : <div><SuperInputText value={name ? name : ''}
-                                           onChangeText={setName}/>
+                {/* user && profileError.length === 0 &&*/}
+                {success &&
+                <>
+                    <div className={s.imgWrap}>
+                        <img className={s.profileImg} style={{width: '100px', height: '100px'}} src={user ? user.avatar : ''} alt={'userPhoto'}/>
                     </div>
-                }
-                <button onClick={updateProfileHandler} disabled={loading}>{change ? 'save' : 'change'}</button>
-                <hr></hr>
+                    {change &&
+                    <div className={s.inputWrap}>
+                        <input className={s.profileInput} type='file' onChange={event => uploadImage(event)}/>
+                    </div>
+                    }
 
-                {/*//@ts-ignore*/}
-                {user && Object.keys(user).filter((key) => key !== 'avatar').map((key, index) => <p key={index}><b>{key}:</b>{user[key]}</p>)}
-            </>
-            }
+                    {!change
+                        ? <div className={s.name} >{user && user.name}</div>
+                        : <div className={s.changeWrap}>
+                            <SuperInputText className={s.changeInput} value={name ? name : ''}onChangeText={setName}/>
+
+                        </div>
+                    }
+                    <button className={s.btnSave} onClick={updateProfileHandler} disabled={loading}>{change ? 'save' : 'change'}</button>
+
+                    <div className={s.dataWrap}>
+                    {/*//@ts-ignore*/}
+                    {user && Object.keys(user).filter((key) => key !== 'avatar').map((key, index) => <p className={s.dataItem} key={index}><b>{key}:</b>{user[key]}</p>)}
+                    </div>
+                </>
+                }
+            </div>
         </div>
     )
+
 })
